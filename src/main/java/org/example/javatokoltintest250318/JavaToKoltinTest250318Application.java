2bfd8a7924dc2a9e8709c3a99e74c5478c43c929
ohnoesganj <sunho3456@naver.com>
2025-03-18 11:55:56 +0900

    Initial Commit

---
+++ src/main/java/org/example/javatokoltintest250318/JavaToKoltinTest250318Application.java
@@ -0,0 +1,13 @@
+package org.example.javatokoltintest250318;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToKoltinTest250318Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToKoltinTest250318Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/javatokoltintest250318/JavaToKoltinTest250318ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javatokoltintest250318;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaToKoltinTest250318ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
